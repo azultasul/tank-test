bc69044f50d251161a060b592b333c1f73ff72e1
azultasul <azultasul@gmail.com>
2025-04-10 00:22:06 +0900

    '

---
+++ app.py
@@ -0,0 +1,13 @@
+import model.augmentation as aug
+
+def main():
+    aug
+    print("👋 Hello! This is the main entry point of app.py")
+
+    # 여기에 본문 로직을 작성하면 됩니다.
+    # 예: 모델 로딩, 이미지 처리, 객체 탐지 등
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# tank_simulator_dataset > 2025-04-09 9:57pm
+https://universe.roboflow.com/tanktest/tank_simulator_dataset
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
